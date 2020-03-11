ed4392eef7d0c9e6ebf75bc1fd3cf04deca2c1f9
tyroneofsea <tyroneofsea@gmail.com>
2020-03-11 17:17:26 +0800

    end_url修复

--- 999_some_tiny_program/biquge_init_spider/book_spider.py
+++ 999_some_tiny_program/biquge_init_spider/book_spider.py
@@ -6,6 +6,7 @@ import random
 import re
 from book_spider_pymongo import SpiderMongo
 import socket
+import time
 
 
 class BookInitSpider(object):
@@ -58,6 +59,7 @@ class BookInitSpider(object):
         print("spidering url =========>",  use_url)
         socket.setdefaulttimeout(self.timeout)
         proxy = self.get_proxy()
+        print(proxy)
         proxies = {
             'socket':  proxy,
             'socket5': proxy
@@ -72,7 +74,7 @@ class BookInitSpider(object):
             'User-Agent': USER_AGENTS[random.randint(0, 19)]
         }
         # headers获取完毕
-        print("headers = ", headers)
+        # print("headers = ", headers)
         try:
             req = urllib.request.Request(url=use_url, headers=headers)
             response = urllib.request.urlopen(req)
@@ -182,6 +184,7 @@ class BookInitSpider(object):
             book_id = re.findall(r"\d+\.?\d*", url_str)[0]
             # 获得要去爬取的图书的ID
             first_url = self.get_book_infos(url=url_str, book_class=book_class, book_id=book_id)
+            # 返回的是第一页地址
             print("I am OK")
             # 查询并记录该小说的ID、书名、作者、状态、最后更新时间、最后一章URL、描述、分类
             # detail_url第一页的内容
@@ -190,13 +193,22 @@ class BookInitSpider(object):
             while True:
                 next_url = self.get_book_details(first_url, book_id, book_capter_numb)
                 book_capter_numb = book_capter_numb + 1
-                if first_url == next_url:
-                    break
+                if url_str == next_url:
+                    print(url_str,'========================',next_url)
+                    print("因为最后的地址指向图书首页，所以这本数爬取完毕")
+                    print("因为最后的地址指向图书首页，所以这本数爬取完毕")
+                    print("因为最后的地址指向图书首页，所以这本数爬取完毕")
+                    print("因为最后的地址指向图书首页，所以这本数爬取完毕")
+                    return
                 else:
+                    print(first_url,'<<<<<<<<<<<<<<<<<<<<<<<<',next_url)
                     first_url = next_url
             # 查询并记录该小说的ID、第几章、该章内容
         except:
-            print("我不应该出现")
+            print("我出现意味着一部小说爬完了")
+            print("我出现意味着一部小说爬完了")
+            print("我出现意味着一部小说爬完了")
+            print("我出现意味着一部小说爬完了")
 
     def get_spider_urls(self, url, book_class):
         html = self.get_html(url)
